faa7019c79d77a89b4f1f2439f6a851b8d113f3d
Carmencc <carmencai5114@gmail.com>
2025-01-22 22:38:21 -0500

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,33 @@
-import styled, { css } from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-// Tag template literal, use ${} to write js code
-const test = css`
-  text-align: center;
-`;
-const StyledApp = styled.div`
-  padding: 20px;
-  ${test}
-`;
 
 function App() {
   return (
     <>
-      {/* The global styled component needs to be sibling, 
-      cannot have children */}
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World</Heading>
+      <BrowserRouter>
+        <Routes>
+          {/* Default route */}
+          <Route index element={<Navigate replace to="dashboard" />} />
 
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert("Check in")}>Check in</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check out")}
-              >
-                Check in
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="Cabins" element={<Cabins />} />
+          <Route path="Users" element={<Users />} />
+          <Route path="Settings" element={<Settings />} />
+          <Route path="Account" element={<Account />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
