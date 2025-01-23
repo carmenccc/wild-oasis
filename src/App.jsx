7d3a5a80f1ff78d4e9e80c0b171722a2ca5d134e
Carmencc <carmencai5114@gmail.com>
2025-01-22 21:37:22 -0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+// Tag template literal
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20;
+`;
+
+function App() {
+  return (
+    <>
+      {/* The global styled component needs to be sibling, cannot have children */}
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello World</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check in</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
