3f77ad1a10ca3db875bc72f7ccfdf59a24780424
Carmencc <carmencai5114@gmail.com>
2025-01-22 22:26:06 -0500

    Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-// Tag template literal
-
+// Tag template literal, use ${} to write js code
+const test = css`
+  text-align: center;
+`;
 const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20;
+  padding: 20px;
+  ${test}
 `;
 
 function App() {
   return (
     <>
-      {/* The global styled component needs to be sibling, cannot have children */}
+      {/* The global styled component needs to be sibling, 
+      cannot have children */}
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello World</H1>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check in</Button>
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello World</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check in
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
