dd0a839363d8487e419733fabe8a9e0936a978c9
Carmencc <carmencai5114@gmail.com>
2025-01-25 18:05:21 -0500

    useOutsideClick

--- src/pages/AddCabin.jsx
+++ src/pages/AddCabin.jsx
@@ -1,26 +1,20 @@
 import Button from "../ui/Button";
 import Modal from "../ui/Modal";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import CabinTable from "../features/cabins/CabinTable";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
 
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
