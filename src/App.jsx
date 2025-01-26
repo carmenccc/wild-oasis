f9d48d249762196a1044ed7b024db1c1b5bf74ab
Carmencc <carmencai5114@gmail.com>
2025-01-26 16:07:36 -0500

    Booking detail page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,6 +35,7 @@ function App() {
 
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="Cabins" element={<Cabins />} />
             <Route path="Users" element={<Users />} />
             <Route path="Settings" element={<Settings />} />
