c543d0efb1676d06a804c41c6811e110edd033f7
Carmencc <carmencai5114@gmail.com>
2025-01-27 15:55:49 -0500

    Bugfix

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("carmen@sample.com");
-  const [password, setPassword] = useState("sample");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
