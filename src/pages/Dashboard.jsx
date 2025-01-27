afd36c4b90fef7b79d334ba95800bae06926917e
Carmencc <carmencai5114@gmail.com>
2025-01-27 13:11:06 -0500

    Dashboard: statistics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,26 @@
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Spinner from "../ui/Spinner";
 
 function Dashboard() {
+  const { bookings, isLoading } = useRecentBookings();
+
+  if (isLoading) return <Spinner />;
+
+  console.log(bookings);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 }
 
