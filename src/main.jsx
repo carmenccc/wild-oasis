9f6f185d8aab45f48dae39314beb251a897e7db2
Carmencc <carmencai5114@gmail.com>
2025-01-27 15:24:11 -0500

    Error Boundaries

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      {/* reset function: can only use regular dom function here, not react-dom function */}
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
